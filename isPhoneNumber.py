b423671aaba2fa3fd1d3600a6cd381b8a2719670
Jae <ssamjang99@gmail.com>
2020-07-07 17:46:39 +1000

    regex practice added to isPhoneNumber.py

---
+++ isPhoneNumber.py
@@ -0,0 +1,17 @@
+#! python3
+# Checks if a string matches a specific pattern (phone number format)
+
+#regex
+import re 
+
+text = 'My number is 415-555-4242'
+
+def isPhoneNumber(text):
+    phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
+    mo = phoneNumRegex.search(text)
+    if not mo:
+        print('No phone numbers found')
+    else:
+        print('Phone number found: ' + mo.group())
+
+isPhoneNumber(text)
