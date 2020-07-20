058089cfae4da8ca155cd5c49f013c79cbb3cc22
ssamjang <ssamjang99@gmail.com>
2020-07-20 23:36:38 +1000

    added email regex for phoneAndEmail.py

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -2,7 +2,8 @@
 # phoneAndEmail.py - Finds phone numbers and email addresses
 # on the clipboard
 
-import pyperclip, re
+import pyperclip
+import re
 
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?          # area code
@@ -15,6 +16,21 @@ phoneRegex = re.compile(r'''(
     (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
     )''', re.VERBOSE)
 
-# TODO: Create email regex
+# email regex
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+       # username (alphanumeric and ._%+-) + means any of the preceding group
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # domain name
+    (\.[a-zA-Z]{2,4})       # dot-something. {2,4} = match at least 2 and at most 4 of something
+    )''', re.VERBOSE)
 
-# TODO: Copy results to the clipboard
+# Find matches in clipboard text
+text = str(pyperclip.paste())
+matches = []
+for group in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
