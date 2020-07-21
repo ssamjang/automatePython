eca425e8f980c016d0dab551a5edc804149be1a0
Jae <ssamjang99@gmail.com>
2020-07-21 15:05:23 +1000

    Finished copy to clipboard

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -27,10 +27,18 @@ emailRegex = re.compile(r'''(
 # Find matches in clipboard text
 text = str(pyperclip.paste())
 matches = []
-for group in phoneRegex.findall(text):
+for groups in phoneRegex.findall(text):
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
     if groups[8] != '':
         phoneNum += ' x' + groups[8]
     matches.append(phoneNum)
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
+
+# Copy results to clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
